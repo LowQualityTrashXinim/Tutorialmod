21850904a96c602e9aa04021ff6ccc95156439b2
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2022-11-03 14:25:50 +0700

    Port StarBook

--- Items/Weapon/MaBookClass/StarBook.cs
+++ Items/Weapon/MaBookClass/StarBook.cs
@@ -3,6 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using static Terraria.ModLoader.ModContent;
+using Terraria.DataStructures;
 
 namespace Tutorialmod.Items.Weapon.MaBookClass
 {
@@ -13,46 +14,48 @@ namespace Tutorialmod.Items.Weapon.MaBookClass
 		}
 
 		public override void SetDefaults() {
-			Item.damage = 6;
-			Item.magic = true;
-			Item.mana = 7;
+			
 			Item.width = 29;
 			Item.height = 32;
+
 			Item.useTime = 10;
             Item.useAnimation = 20;
-            Item.crit = 7;
-            Item.useStyle = 5;
-			Item.useStyle = ItemUseStyleID.Shoot;
-			Item.noMelee = true;
+            
+			Item.mana = 7;
+			Item.crit = 7;
+			Item.damage = 6;
+			Item.shootSpeed = 20f;
 			Item.knockBack = 1.5f;
 			Item.value = 10000;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+			Item.DamageType = DamageClass.Magic;
+			Item.useStyle = ItemUseStyleID.Shoot;
 			Item.rare = ItemRarityID.Blue;
-			Item.UseSound = SoundID.Item20;
-			Item.autoReuse = true;
 			Item.shoot = ProjectileType<StarBookP>();
-			Item.shootSpeed = 20f;
+			Item.noMelee = true;
+			Item.autoReuse = true;
 		}
 
 		public override void AddRecipes() {
-			ModRecipe recipe = new ModRecipe(Mod);
-            recipe.AddIngredient(ItemID.Book, 10);
-            recipe.AddIngredient(ItemID.FallenStar, 15);
-            recipe.AddIngredient(ItemID.ManaCrystal, 3);
-            recipe.AddIngredient(ItemID.Goldfish, 1);
-            recipe.AddTile(TileID.Bookcases);
-            recipe.SetResult(this);
-			recipe.AddRecipe();
+			CreateRecipe()
+			.AddIngredient(ItemID.Book, 10)
+			.AddIngredient(ItemID.FallenStar, 15)
+			.AddIngredient(ItemID.ManaCrystal, 3)
+			.AddIngredient(ItemID.Goldfish, 1)
+			.AddTile(TileID.Bookcases)
+			.Register();
 		}
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
 			int numberprojectile = 1 + Main.rand.Next(1, 3);
 			for (int i = 0; i < numberprojectile; i++)
 			{
-				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
-				speedX = perturbedSpeed.X;
-				speedY = perturbedSpeed.Y;
-				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+				Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(10));
+				velocity.X = perturbedSpeed.X;
+				velocity.Y = perturbedSpeed.Y;
+				Projectile.NewProjectile(source,position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockback, player.whoAmI);
 			}
 			return false;
 		}
